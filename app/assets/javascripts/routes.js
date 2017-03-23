d06918897595b3b9f9e4c7f4ac5bcfd478fb2f39
Samuel Monroe <spat.monroe@gmail.com>
2017-03-23 23:29:02 +0100

    Continuing work

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,16 +1,12 @@
 angular.module('canonball').config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider,$locationProvider){
     $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: 'index.html'
-        })
         .state('youtube', {
             url: '/youtube',
             templateUrl: 'youtube.html',
             controller: 'YoutubeController'
         })
         .state('twitch', {
-            url: '/twitch',
+            url: '/',
             templateUrl: 'twitch.html'
         });
     $locationProvider.html5Mode({enabled: true, requireBase: false});
