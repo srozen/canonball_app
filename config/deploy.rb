2869bae71a4a548b1ab5faa4eddcebedd33c67cf
Samuel Monroe <spat.monroe@gmail.com>
2017-06-25 22:56:06 +0200

    Trying to solve assets issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -87,20 +87,7 @@ namespace :deploy do
     ENV['REASON'] ||= "Deployment of new version"
     ENV['UNTIL'] ||= 10.minutes.from_now.to_datetime.to_formatted_s(:rfc822)
   end
-
-  namespace :bower do
-    desc 'Install bower'
-    task :install do
-      on roles(:web) do
-        within release_path do
-          with rails_env: fetch(:rails_env) do
-            execute :rake, 'bower:install CI=true'
-          end
-        end
-      end
-    end
-  end
-  before 'deploy:compile_assets', 'bower:install'
+  
 
 
 end
