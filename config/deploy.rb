19bdc3721c96e4e39b5ff5f4412d74e657feff9c
Samuel Monroe <spat.monroe@gmail.com>
2017-06-25 16:08:07 +0200

    Fixin Puma socket

--- config/deploy.rb
+++ config/deploy.rb
@@ -78,6 +78,17 @@ set :puma_preload_app, true
 set :maintenance_template_path, 'app/views/maintenance.html.erb'
 set :maintenance_config_warning, false
 
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
 
 namespace :deploy do
 
@@ -108,6 +119,24 @@ namespace :deploy do
       end
     end
   end
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
 
 
 end
