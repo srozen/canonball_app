803ee9a177bc5e9cb25b81d76ead8c6901fcaeda
Samuel Monroe <spat.monroe@gmail.com>
2017-06-25 15:01:14 +0200

    Complete configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :repo_url,  "git@github.com:srozen/canonball_app.git"
 # set :deploy_to, '/var/www/my_app_name'
 
 # Default value for :scm is :git
-set :scm, :git
+# set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
